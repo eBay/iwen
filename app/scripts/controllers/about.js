600fe9a104573adc1cca44248c0275a41e94e8c0
atopper <atopper@ebay.com>
2016-12-23 14:48:50 -0800

    even more setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name iwenApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the iwenApp
+ */
+angular.module('iwenApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name iwenApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the iwenApp
+ */
+angular.module('iwenApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
