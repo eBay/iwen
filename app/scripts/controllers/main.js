d82a8b42c92dc97c65afb06de3156fa3141bdf2d
An-Ni Wang <annwang@ebay.com>
2018-05-14 15:30:06 -0700

    Revert "updating styles"
    This reverts commit 1cf25de813372353843c4757d8fb5b42cfd39220.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -246,21 +246,15 @@ angular.module('iwenApp')
 
     function handlers(){
        $('aside li').on('click', function(){
-          $('aside li').removeClass('active');
-          $(this).addClass('active');
+          $('aside li h3').removeClass('active');
+          $(this).find('h3').addClass('active');
        });
 
-       $('#font').fontselect().change(function(){
+      $('#font').fontselect().change(function(){
             var font = $(this).val().replace(/\+/g, ' ');
             font = font.split(':');
             $scope.util.fontSelected(font[0]);
-       });
-
-       $('.palette__pairings').on('click', '.card', function(){
-          $('.palette__pairings .card').removeClass('active');
-          $(this).addClass('active');
-       })
-
+      });
     }
 
 
